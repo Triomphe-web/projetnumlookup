2d06750555b00a8a8dbb0b6e13f3ac1a4e0b98c9
akane <f23triomphe@gmail.com>
2023-02-20 22:27:36 +0100

    Update

---
+++ index.js
@@ -0,0 +1,26 @@
+//code javascriipt
+
+document.getElementById("clickme").addEventListener("click", function () {
+  let tel = document.getElementById('tel').value
+  console.log(tel);
+  const request = new XMLHttpRequest();
+  const url =
+    `https://api.numlookupapi.com/v1/validate/${tel}?apikey=gK0Xj3Pe0Ob7Rp8DfnGBm5YGRuumJG5DIyVsi8Al;`
+
+  request.open("GET", url);
+  request.send();
+
+  request.onload = function () {
+    if (request.status === 200) {
+      //parse de json
+      data = JSON.parse(request.responseText);
+      document.getElementById("p1").innerHTML = data.number
+      document.getElementById("p2").innerHTML = data.country_name
+      document.getElementById("p3").innerHTML = data.country_code
+      document.getElementById("p4").innerHTML = data.carrier
+      document.getElementById("p5").innerHTML = data.line_type
+    } else if (request.status === 404) {
+      console.log("not found");
+    }
+  };
+});
